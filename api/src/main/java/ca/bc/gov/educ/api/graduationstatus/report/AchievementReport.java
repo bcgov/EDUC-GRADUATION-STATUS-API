33a6dd07a81501d23a4ff39d361688a631e6bff8
Kamal Mohammed <kamal.mohammed@gov.bc.ca>
2020-03-09 12:28:18 -0700

    Code Updates

--- api/src/main/java/ca/bc/gov/educ/api/graduationstatus/report/AchievementReport.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduationstatus/report/AchievementReport.java
@@ -17,146 +17,62 @@ public class AchievementReport implements StudentReport {
     public void buildAchievementReportHtml() {
         html.append("<!DOCTYPE html>\n" +
                 "<html>\n" +
-                "      \n" +
-                "      <head>\n" +
-                "         <style>\n" +
-                "            body {\n" +
-                "            font-size: .5em;\n" +
-                "            padding: 20px;\n" +
-                "            width: 90%;\n" +
-                "            }\n" +
-                "            table {\n" +
-                "            border-collapse: collapse;\n" +
-                "            width: 100%;\n" +
-                "            }\n" +
-                "            td, th {\n" +
-                "            border: 1px solid #dddddd;\n" +
-                "            text-align: left;\n" +
-                "            padding: 8px;\n" +
-                "            }\n" +
-                "            tr:nth-child(even) {\n" +
-                "            background-color: #dddddd;\n" +
-                "            }\n" +
-                "         </style>\n" +
-                "      </head>\n" +
-                "      <body>\n" +
-                "\t\t  <! Header Information>\n" +
-                "      <h1 style=\"text-align: center;\"><strong>Ministry of Education</strong></h1>\n" +
-                "      <h2 style=\"text-align: center;\"><strong>Student Achievement Report</strong></h2>\n" +
-                "      <p style=\"text-align: center;\"><strong>(Transcript Verification)</strong></p>\n" +
-                "      <p style=\"text-align: center;\">Date: 2020-02-20</p>\n" +
-                "      <! Student Information>\n" +
-                "      <h2>Student</h2>\n" +
-                "      <p><strong>Matthews, John Middle</strong></p>\n" +
-                "      <p>PEN: 123456789</p>\n" +
-                "      <p>Graduation Program : 2018</p>\n" +
-                "      <p>School: Oak Bay High School</p>\n" +
-                "      <p>Date of Birth: 2009-06-12</p>\n" +
-                "      <p>Grade: 12</p>\n" +
-                "      <p>Gender: M</p>\n" +
-                "      <p>Citizenship: C</p>\n" +
-                "      <p>Address: 1234 Sunshine Way, Victoria BC V8V 4Y9</p>\n" +
-                "      <! Student Grad Status>\n" +
-                "      <h2 style=\"color: blue\">Student Graduation Status </h2>\n" +
-                "      <h3 style=\"color: blue\">This student has not yet graduated in the 2018 graduation program.</h3>\n" +
-                "      <! Student List of Courses and Assessments>\n" +
-                "         <h2>Student Course / Assessment </h2>\n" +
-                "         <table>\n" +
-                "            <tr>\n" +
-                "            <tr style=\"height: 39px;\">\n" +
-                "               <th>Course / Assessment Name</th>\n" +
-                "               <th> Course / Assessment Code</th>\n" +
-                "               <th> Graduation Requirement Met</strong</th>\n" +
-                "               <th> Course Type</th>\n" +
-                "               <th> Session Date</th>\n" +
-                "               <th> Interim %</th>\n" +
-                "               <th> Final %</th>\n" +
-                "               <th> Final Letter Grade</th>\n" +
-                "               <th> Credits</td>\n" +
-                "               <th> Comments</th>\n" +
+                "\t<head>\n" +
+                "\t <style>\n" +
+                "\t    body {font-size: .5em; padding: 20px; width: 90%; } table {border-collapse: collapse; width: 100%; }\n" +
+                "\t    td, th {border: 1px solid #dddddd; text-align: left; padding: 8px; } tr:nth-child(even) {background-color: #dddddd; }\n" +
+                "\t </style>\n" +
+                "\t</head>\n" +
+                "\t<body>\n" +
+                "\t\t<! Header Information>\n" +
+                "    \t<h1 style=\"text-align: center;\"><strong>Ministry of Education</strong></h1>\n" +
+                "    \t<h2 style=\"text-align: center;\"><strong>Student Achievement Report</strong></h2>\n" +
+                "    \t<p style=\"text-align: center;\"><strong>(Transcript Verification)</strong></p>\n" +
+                "    \t<p style=\"text-align: center;\">Date: 2020-02-20</p>\n" +
+                "    \t<! Student Information>\n" +
+                "    \t<h2>Student</h2>\n" +
+                "    \t<p><strong>Matthews, John Middle</strong></p>\n" +
+                "    \t<p>PEN: 123456789</p>\n" +
+                "    \t<p>Graduation Program : 2018</p>\n" +
+                "    \t<p>School: Oak Bay High School</p>\n" +
+                "    \t<p>Date of Birth: 2009-06-12</p>\n" +
+                "    \t<p>Grade: 12</p>\n" +
+                "    \t<p>Gender: M</p>\n" +
+                "    \t<p>Citizenship: C</p>\n" +
+                "    \t<p>Address: 1234 Sunshine Way, Victoria BC V8V 4Y9</p>\n" +
+                "    \t<! Student Grad Status>\n" +
+                "    \t<h2 style=\"color: blue\">Student Graduation Status </h2>\n" +
+                "    \t<h3 style=\"color: blue\">This student has not yet graduated in the 2018 graduation program.</h3>\n" +
+                "    \t<! Student List of Courses and Assessments>\n" +
+                "        <h2>Student Course / Assessment </h2>\n" +
+                "        <table>\n" +
+                "        \t<tr style=\"height: 39px;\">\n" +
+                "               <th>Course / Assessment Name</th> <th> Course / Assessment Code</th> <th> Graduation Requirement Met</strong</th> <th> Course Type</th> <th> Session Date</th> <th> Interim %</th> <th> Final %</th> <th> Final Letter Grade</th> <th> Credits</td> <th> Comments</th> \n" +
                 "            </tr>\n" +
                 "            <tr>\n" +
-                "               <td>English 10</td>\n" +
-                "               <td>EN 10</td>\n" +
-                "               <td>101</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>2016-06</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>100</td>\n" +
-                "               <td>A</td>\n" +
-                "               <td>4</td>\n" +
-                "               <td>&nbsp;</td>\n" +
+                "               <td>English 10</td> <td>EN 10</td> <td>101</td> <td>&nbsp;</td> <td>2016-06</td> <td>&nbsp;</td> <td>100</td> <td>A</td> <td>4</td> <td>&nbsp;</td>\n" +
                 "            </tr>\n" +
                 "            <tr>\n" +
-                "               <td>English 10</td>\n" +
-                "               <td>EN 10</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>2017-06</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>88</td>\n" +
-                "               <td>A</td>\n" +
-                "               <td>4</td>\n" +
-                "               <td>duplicate</td>\n" +
+                "               <td>English 10</td> <td>EN 10</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>2017-06</td> <td>&nbsp;</td> <td>88</td> <td>A</td> <td>4</td> <td>duplicate</td>\n" +
                 "            </tr>\n" +
                 "            <tr>\n" +
-                "               <td>Physical Education 10</td>\n" +
-                "               <td>PE 10</td>\n" +
-                "               <td>110</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>2018-11</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>100</td>\n" +
-                "               <td>A</td>\n" +
-                "               <td>4</td>\n" +
-                "               <td>&nbsp;</td>\n" +
+                "               <td>Physical Education 10</td> <td>PE 10</td> <td>110</td> <td>&nbsp;</td> <td>2018-11</td> <td>&nbsp;</td> <td>100</td> <td>A</td> <td>4</td><td>&nbsp;</td>\n" +
                 "            </tr>\n" +
                 "            <tr>\n" +
-                "               <td>Science 10</td>\n" +
-                "               <td>SC 10</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>2016-01</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>44</td>\n" +
-                "               <td>F</td>\n" +
-                "               <td>4</td>\n" +
-                "               <td>fail</td>\n" +
-                "            </tr>\n" +
+                "               <td>Science 10</td> <td>SC 10</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>2016-01</td> <td>&nbsp;</td> <td>44</td> <td>F</td> <td>4</td> <td>fail</td> </tr>\n" +
                 "            <tr>\n" +
-                "               <td>Social Studies 11</td>\n" +
-                "               <td>SS 11</td>\n" +
-                "               <td>105</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>2018-01</td>\n" +
-                "               <td>&nbsp;</td>\n" +
-                "               <td>100</td>\n" +
-                "               <td>A</td>\n" +
-                "               <td>4</td>\n" +
-                "               <td>&nbsp;</td>\n" +
+                "               <td>Social Studies 11</td> <td>SS 11</td> <td>105</td> <td>&nbsp;</td> <td>2018-01</td> <td>&nbsp;</td> <td>100</td>  <td>A</td> <td>4</td> <td>&nbsp;</td>\n" +
                 "            </tr>\n" +
-                "         </table><br/><br/>\n" +
-                "         <hr/><br/><br/>\n" +
-                "         <! Student List of sub-programs>\n" +
-                "         <h2>Ancillary Programs</h2>\n" +
-                "         <ul>\n" +
-                "            <li>Advanced Placement</li>\n" +
-                "            <li>Career Program: Agriculture</li>\n" +
-                "         </ul>\n" +
-                "         <! Student List of Requirements Met>\n" +
-                "         <h2>Graduation Program 2018 Requirements Met</h2>\n" +
-                "         <ul>\n" +
-                "            <li>Language Arts 10</li>\n" +
-                "            <li>Physical and Health Education 10</li>\n" +
-                "            <li>Social Studies 11 or 12 </li>\n" +
-                "         </ul>\n" +
-                "         <! Student List of Requirements Not Met>\n" +
-                "         <h2>Graduation Program 2018 Requirements Not Met</h2>\n" +
-                "         <ul>\n" +
-                "            <li>No Language Arts 12</li>\n" +
-                "            <li>No Literacy 10 Assessment</li>\n" +
-                "            <li>Fewer than 16 Grade 12 credits</li>\n" +
-                "         </ul>\n" +
+                "        </table><br/><br/><hr/><br/><br/>\n" +
+                "        <! Student List of sub-programs>\n" +
+                "        <h2>Ancillary Programs</h2>\n" +
+                "        <ul> <li>Advanced Placement</li> <li>Career Program: Agriculture</li> </ul>\n" +
+                "        <! Student List of Requirements Met>\n" +
+                "        <h2>Graduation Program 2018 Requirements Met</h2>\n" +
+                "        <ul> <li>Language Arts 10</li> <li>Physical and Health Education 10</li> <li>Social Studies 11 or 12 </li> </ul>\n" +
+                "        <! Student List of Requirements Not Met>\n" +
+                "        <h2>Graduation Program 2018 Requirements Not Met</h2>\n" +
+                "        <ul> <li>No Language Arts 12</li> <li>No Literacy 10 Assessment</li> <li>Fewer than 16 Grade 12 credits</li> </ul>\n" +
                 "   </body>\n" +
                 "</html>");
     }
